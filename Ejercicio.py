d17e7e98cf549b69ba9052e454b609a2b19612ba
MarkitosUS <markitosurrea97@gmail.com>
2022-06-09 16:10:13 +0200

    Modulos

---
+++ Ejercicio.py
@@ -0,0 +1,6 @@
+from Operaciones import Operaciones
+
+print("2 + 2 =", Operaciones.suma(2,2) )
+print("2 - 2 =", Operaciones.resta(2,2))
+print("2 * 2 =", Operaciones.multiply(2,2))
+print("2 / 2 =", Operaciones.divide(2,2))
---
+++ Operaciones.py
@@ -0,0 +1,12 @@
+class Operaciones:
+    def suma(x, y):
+            return x + y
+
+    def resta(x, y):
+            return x - y
+
+    def multiply(x, y):
+            return x * y
+
+    def divide(x, y):
+            return x / y
